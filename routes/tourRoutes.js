a4b0f9116186d53120ae2e905563b2dd2803648e
vyom-chaudhary <vyom@itservicesindia.com>
2020-06-04 11:55:41 +0530

    changes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -26,7 +26,7 @@ router
 
 router
   .route('/')
-  .get(tourController.getAllTours)
+  .get(authController.protect, tourController.getAllTours)
   .post(
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
